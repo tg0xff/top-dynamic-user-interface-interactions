2f14f844543dadabeb3466ca7cc58c7f9eae8971
Selene Tamez Galván <tg.sln255@gmail.com>
2024-07-01 17:09:42 -0600

    Make script reusable

--- dropdown-menu/index.js
+++ dropdown-menu/index.js
@@ -1,16 +1,14 @@
-const btn = document.querySelector("button.dropdown");
-const menu = btn.querySelector("ol");
-let btnToggle = false;
-
-function showDropdownMenu() {
-  if (btnToggle) {
+function toggleDropdownMenu(e) {
+  const btn = e.target;
+  const menu = btn.querySelector("ol");
+  if (!btn.classList.contains("active")) {
     btn.classList.add("active");
     menu.classList.add("visible");
   } else {
     btn.classList.remove("active");
     menu.classList.remove("visible");
   }
-  btnToggle = !btnToggle;
 }
 
-btn.addEventListener("click", showDropdownMenu);
+const btns = document.querySelectorAll("button.dropdown");
+btns.forEach((btn) => btn.addEventListener("click", toggleDropdownMenu));
