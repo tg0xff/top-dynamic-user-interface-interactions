0f99f62bc7e25b44986154505e4c06311d2e755f
tg0xff <tg0xff@proton.me>
2024-07-02 15:26:00 -0600

    Create an obj per carousel

--- carousel/index.js
+++ carousel/index.js
@@ -45,3 +45,7 @@ class ImageCarousel {
     }
   }
 }
+
+const carousels = document.querySelectorAll("div.carousel");
+const nodes = [...carousels];
+nodes.forEach((elem) => new ImageCarousel(elem));
